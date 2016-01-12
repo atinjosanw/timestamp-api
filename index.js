d65460ed09aa9460b5f23c51bfe2608bec68851b
Yarting <atinjosan@yahoo.com>
2016-01-12 18:32:42 +0000

    refactor index.js/server code. - use middleware router to route request - set time string interpretation to controllers/msg.js

--- index.js
+++ index.js
@@ -1,49 +1,39 @@
 'use strict';
 
+// BASE SETUP
+// =============================================================================
+
 var express = require('express');
-
 var app = express();
-
-app.get('/:ts', function (req, res) {
-	var timestampString = req.params.ts;
-	var timestamp = {"unix":null, "natural": null}, r;
-	
-	var monthList = ['January', 'February', 'March', 'April', 'May', 'June', 'July',
-						'August', 'September', 'October', 'November', 'December'];
-	var numDays = [31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
-	function getUnix (tsStr) {
-		var ts = new Date(tsStr * 1000);
-		var year = ts.getFullYear();
-		var month = monthList[ts.getMonth()];
-		var day = ts.getDate();
-		return {"unix": +tsStr, "natural": month + " " + day + ", " + year};
-	}
-	function getNatural (m) {
-		var month = m[1], date = m[2], year = m[3];
-		var capMonth = month[0].toUpperCase() + month.slice(1);
-		var nm = monthList.indexOf(capMonth);
-		if ( nm  == -1 || +year < 1970)
-			return null;
-		var nd = nm == 1 ? (+year % 4 == 0 ? 28 : 29) : numDays[nd];
-		if (+date > nd)
-			return null;
-		var naturalTime = capMonth + " " + date + ", " + year;
-		return {"unix": Math.round(+new Date(naturalTime).getTime() / 1000), "natural": naturalTime };
-	}
-
-	var m = timestampString.match(/^([A-Za-z]+) (\d{1,2}), (\d{4})$/);
-	
-	if (/^\d{10}$/.test(timestampString)) {
-		r = getUnix(timestampString);
-	}
-	else if (m) {
-		r = getNatural(m);
-	}
-	res.json(r || timestamp);
-
-});
+var Msg = require('./controllers/msg');
 
 var port = process.env.PORT || 8080;
+
+var router = express.Router();
+
+router.use(function (req, res, next) {
+	console.log('Receive connection.');
+	next();
+});
+
+router.get('/', function (req, res) {
+	res.json({ message: "This is a timestamp API in node!"});
+});
+
+router.route('/:ts').get( function (req, res) {
+	Msg.interpretTime(req.params.ts, function (err, time) {
+		if (err) {
+			res.send(err);
+			return;
+		}
+		res.json(time);
+	});
+});
+
+app.use('/', router);
+
 app.listen(port,  function () {
 	console.log('Node.js listening on port ' + port + '...');
 });
+
+
