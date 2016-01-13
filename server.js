f4a9a8753d166be91e5e90e6eb594d984b948a19
Yarting <atinjosan@yahoo.com>
2016-01-13 06:23:03 +0000

    Edit server.js, it is for bootstraping the service

--- server.js
+++ server.js
@@ -2,38 +2,12 @@
 
 // BASE SETUP
 // =============================================================================
+var app = require('./app');
 
-var express = require('express');
-var app = express();
-var Msg = require('./controllers/msg');
+app.set('port', process.env.PORT || 8000);
 
-var port = process.env.PORT || 8080;
-
-var router = express.Router();
-
-router.use(function (req, res, next) {
-	console.log('Receive connection.');
-	next();
-});
-
-router.get('/', function (req, res) {
-	res.json({ message: "This is a timestamp API in node!"});
-});
-
-router.route('/:ts').get( function (req, res) {
-	Msg.interpretTime(req.params.ts, function (err, time) {
-		if (err) {
-			res.send(err);
-			return;
-		}
-		res.json(time);
-	});
-});
-
-app.use('/', router);
-
-app.listen(port,  function () {
-	console.log('Node.js listening on port ' + port + '...');
+var server = app.listen(app.get('port'),  function () {
+	console.log('Node.js listening on port ' + server.address().port + '...');
 });
 
 
